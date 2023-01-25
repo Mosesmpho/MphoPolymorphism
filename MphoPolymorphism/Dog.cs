3189e2cd16e1750b878f858c407f086b28980049
Mosesmpho <mosesmpho16@gmail.com>
2023-01-25 12:55:30 +0200

    Add project files.

---
+++ MphoPolymorphism/Dog.cs
@@ -0,0 +1,5 @@
+﻿using MphoPolymorphism;
+
+internal class Dog : Animal
+{
+}
---
+++ MphoPolymorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using MphoPolymorphism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();  // Create a Animal object
+        Animal myPig = new Pig();  // Create a Pig object
+        Animal myDog = new Dog();  // Create a Dog object
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
